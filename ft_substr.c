e29b63bca812568d6da784e1b99dd0febb80e30c
youkim <greenscarf005@gmail.com>
2021-05-12 14:29:28 +0900

    substr mko fix

--- ft_substr.c
+++ ft_substr.c
@@ -6,28 +6,31 @@
 /*   By: youkim <youkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 18:48:38 by youkim            #+#    #+#             */
-/*   Updated: 2021/05/10 11:04:44 by youkim           ###   ########.fr       */
+/*   Updated: 2021/05/12 14:20:08 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+static size_t	st_min(size_t first, size_t last)
+{
+	if (first >= last)
+		return (last);
+	return (first);
+}
+
+char			*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
-	size_t	slen;
 
 	if (!s)
 		return (0);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	len = st_min(len, ft_strlen(s + start));
 	sub = malloc((len + 1) * sizeof(char));
 	if (!sub)
 		return (0);
-	slen = ft_strlen(s);
-	if (slen <= start)
-	{
-		sub[0] = 0;
-		return (sub);
-	}
 	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
